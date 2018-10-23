a39da109807146ca8400049c6b29946dd527b7ff
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-23 23:11:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    if Time.now.hour < 12
+    if Time.now.hour < 10
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
